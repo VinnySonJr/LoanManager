573530e039921e097cb02ddd0033aff3b007c48b
parastoo noorossana <parastoo.noorossana@eagles.oc.edu>
2017-11-28 21:33:19 -0600

    Created ui- added test file and created datamanagement package.

---
+++ LoanManager/src/dataManagement/DataManager.java
@@ -0,0 +1,30 @@
+package dataManagement;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class DataManager {
+       private Scanner x;
+       
+       public void openFile(){
+    	   try{
+    		   x = new Scanner(new File("textData.txt"));
+    	   }
+    		   catch(Exception e){
+    			   System.out.print("could not find file");
+    		   }
+        }
+       public void readFile(){
+    	   while(x.hasNext()){
+    		    String a = x.next();
+    		    String b = x.next();
+    		    String c = x.next();
+    		    System.out.println(a);
+    		    System.out.println(b);
+    		    System.out.println(c);
+    	   }
+       }
+       public void closeFile(){
+    	   x.close();
+       }
+}
